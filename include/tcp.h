01ef7fd9bd94a38f838614cf382ac7c8d2181c83
Oleg Reutskiy <o.reutskiy@corp.mail.ru>
2021-10-07 13:11:17 +0300

    fix

--- include/tcp.h
+++ include/tcp.h
@@ -6,12 +6,3 @@
 int accept_socket(int event_fd, struct sockaddr *client_addr, socklen_t *client_len);
 
 int send_response(int event_fd, char *path, char *header);
-
-//int sendlong(int sock, long value);
-
-int senddata(int sock, void *buf, int buflen);
-
-int send_file(int sock, FILE *f);
-
-size_t min(size_t first, size_t second);
-
